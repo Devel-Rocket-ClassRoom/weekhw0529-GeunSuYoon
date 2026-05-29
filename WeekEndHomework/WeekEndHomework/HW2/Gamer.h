75a640ea8037020d2745e5ce7b13d80114bda965
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-30 07:46:29 +0900

    feat: FGamer 스트럭처 내부 함수 및 setter getter 추가

--- WeekEndHomework/WeekEndHomework/HW2/Gamer.h
+++ WeekEndHomework/WeekEndHomework/HW2/Gamer.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "./ConstDefine.h"
 
@@ -16,9 +16,11 @@ struct FGamer
 
 	FGamer()
 	{ }
+	// 딜러로 세팅할 때 사용
 	FGamer(bool InIsPlayer)
 		: IsPlayer(InIsPlayer)
 	{ }
+	// 내부 요소들 초기화
 	void	Reset(void)
 	{
 		for (int CardCount = 0; CardCount < CardDeckSize; CardCount++)
@@ -33,52 +35,70 @@ struct FGamer
 		AScore = 0;
 		Score = 0;
 	}
-	void	Print(void)
+	// 점수와 카드 내용 출력하는 함수
+	void	Print(bool InIsGameEnd)
 	{
 		if (this->CardDeckSize)
 		{
 			if (IsPlayer)
 			{
 				printf("플레이어 현재 점수: [%d]\n", this->Score);
-				printf("%s ", kCardDeck[CardDeck[0]]);
+				printf("[%s] ", kCardDeck[CardDeck[0]]);
 			}
 			else
 			{
-				printf("딜러 현재 점수: [%d]\n", this->Score - kCardPoint[0]);
-				printf("?? ");
+				if (InIsGameEnd)
+				{
+					printf("딜러 현재 점수: [%d]\n", this->Score);
+					printf("[%s] ", kCardDeck[CardDeck[0]]);
+				}
+				else
+				{
+					printf("딜러 현재 점수: [%d]\n", this->Score - kCardPoint[CardDeck[0]]);
+					printf("[??] ");
+				}
 			}
 			for (int CardCount = 1; CardCount < CardDeckSize; CardCount++)
 			{
-				printf("%s ", kCardDeck[CardDeck[CardCount]]);
+				printf("[%s] ", kCardDeck[CardDeck[CardCount]]);
 			}
 			printf("\n");
 		}
 	}
+	// 카드 추가하는 함수
 	void	AddCard(int InCard)
 	{
 		this->CardDeck[CardDeckSize++] = InCard;
 		this->Score += kCardPoint[InCard];
+		// 뽑은 카드가 에이스면 에이스 추가됐다고 말하기
 		if (!(InCard % kShapeCardSize))
 		{
 			this->ASize++;
 		}
-		this->Score -= this->AScore * kAScore;
-		this->AScore = 0;
-		while (this->ASize != this->AScore)
+		// 에이스 카드로 추가점수 얻었으면 추가점수 다 빼주기
+		if (this->AScore)
+		{
+			this->Score -= this->AScore * kAScore;
+			this->AScore = 0;
+		}
+		// 21점이 넘지 않는 선에서 에이스 추가점수 더하기
+		while (this->AScore < this->ASize)
 		{
 			if (this->Score + kAScore > kBurstScore)
 				break ;
 			this->AScore++;
 			this->Score += kAScore;
 		}
-		if (this->Score + kAScore <= kBurstScore)
+		// 버스트 스코어 이하일 때
+		if (this->Score <= kBurstScore)
 		{
-			this->IsBurst = false;
-			if (this->CardDeckSize == 2)
+			// 블랙잭인 경우
+			if (this->CardDeckSize == 2 && this->Score + kAScore == kBurstScore)
 			{
 				IsBlacJack = true;
 			}
 		}
+		// 버스트일 때
 		else
 		{
 			this->IsBurst = true;
@@ -87,5 +107,21 @@ struct FGamer
 	void	SetIsStand(bool InVal)
 	{
 		this->IsStand = InVal;
+	}
+	bool	GetIsBurst(void) const
+	{
+		return (this->IsBurst);
+	}
+	bool	GetIsStand(void) const
+	{
+		return (this->IsStand);
+	}
+	bool	GetIsBlacJack(void) const
+	{
+		return (this->IsBlacJack);
+	}
+	int	GetScore(void) const
+	{
+		return (this->Score);
 	}
 };
