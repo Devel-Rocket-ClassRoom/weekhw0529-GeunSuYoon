8618f6d914c010786c9fc74e1e03bb6bc779537d
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-30 07:57:06 +0900

    feat: Add HW1run continue logic

--- WeekEndHomework/WeekEndHomework/WeekEndHomework.cpp
+++ WeekEndHomework/WeekEndHomework/WeekEndHomework.cpp
@@ -4,6 +4,19 @@
 
 int main()
 {
-    //Homework01_Run();
+	bool	HW1Continue = true;
+
+	srand(time(NULL));
+	while (HW1Continue)
+	{
+		Homework01_Run();
+		printf("계속하시려면 그냥 엔터를 눌러주세요.\n");
+		printf("그만하시려면 아무 키나 입력한 뒤 엔터를 눌러주세요.\n");
+		std::string	InputLine;
+		std::getline(std::cin, InputLine);
+		if (InputLine.size())
+			HW1Continue = false;
+		system("cls");
+	}
     Homework02_Run();
 }
