8a1b779b2bac383aac237ae4cf8a8d2700632274
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-30 07:45:40 +0900

    feat: 출력 좌표 유틸 함수 추가

---
+++ WeekEndHomework/WeekEndHomework/HW2/CoordUtil.cpp
@@ -0,0 +1,22 @@
+﻿#include "./CoordUtil.h"
+
+// 지금 커서 위치를 저장한다.
+COORD	GetCurrentCursorPosition(void)
+{
+	HANDLE						NowConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO	NowCsbi;
+
+	GetConsoleScreenBufferInfo(NowConsole, &NowCsbi);
+	return (NowCsbi.dwCursorPosition);
+}
+
+// 입력한 위치로 커서를 이동한다.
+void	SetCursorPosition(int PosX, int PosY)
+{
+	HANDLE	NowConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD	Pos;
+
+	Pos.X = PosX;
+	Pos.Y = PosY;
+	SetConsoleCursorPosition(NowConsole, Pos);
+}
---
+++ WeekEndHomework/WeekEndHomework/HW2/CoordUtil.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include <windows.h>
+
+// 출력시 콘솔 커서 컨트롤용 유틸
+COORD	GetCurrentCursorPosition(void);
+void	SetCursorPosition(int PosX, int PosY);
